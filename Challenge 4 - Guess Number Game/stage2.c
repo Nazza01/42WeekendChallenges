8871c22c87b8469445fe4e747136c9916a179b0c
Nazza01 <nate_thomas_95@hotmail.com>
2021-06-04 17:29:54 +0930

    Finishing Up

--- Challenge 4 - Guess Number Game/stage2.c
+++ Challenge 4 - Guess Number Game/stage2.c
@@ -1,4 +1,3 @@
-// Stage 2. Create a guess the number game in C
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,11 +5,10 @@
 #include <unistd.h>
 
 // Declare Variables
-char Bar[20] = "####################";
-char ellip[3] = "...";
 int N = 0;
 int ans,guess = 0;
 
+// Stage 2. Create a guess the number game in C
 int main()
 {
     srand ( time(NULL) );                           // Seed the random number generator using time so it's not the same every time the program is executed.
@@ -25,41 +23,21 @@ int main()
         else                                        // If the input is a number, start the game!
         {
             printf("Lets generate a random number between 1 - %i", N);
-            int ans = 1+rand() % N;                   // Generate random number between (+1 as it would use 0 in the guess check) and upper limit  
-            printf("\nChecking :");
-            for (int i =0; i < 19; i++)
-            {
-                sleep(1);                            // Sleep for the loading effect
-                printf("\r%c", Bar[i]);               //
-                fflush(stdout);                       // Flush the standard output as it's being buffered and stored
-            }
-            /*
-            for (int i = 0; i < 19; i++)              // Progress Bar
-            {
-                printf("\r%c", Bar[i]);
-                fflush(stdout);
-                sleep(.1);
-            }
-            */
+            int ans = 1+rand() % N;                 // Generate random number between (+1 as it would use 0 in the guess check) and upper limit  
+            printf("\nChecking");
+            sleep(2);
             printf("\nPlease guess the number!\n");
-            scanf("%i", &guess);                      // Let the player guess the number
-            
+            scanf("%i", &guess);                    // Let the player guess the number
             while ( guess != ans )
             {
-                for (int i = 0; i < 2; i++)
-                {
-                    printf("Checking \r%c", ellip[i]);
-                    sleep(.5);
-                }
+
                 printf("Please guess again!\n");
-                sleep(1);
                 scanf("%i", &guess);
+                printf("Checking..\n");
+                sleep(1);
             }
-            for (int i = 0; i <2; i++)
-            {
-                printf("Checking \r%c", ellip[i]);
-                sleep(0.5);
-            }
+            printf("Checking..\n");
+            sleep(2);
             printf("Well Done! You have guessed correctly!");
         }
     }
