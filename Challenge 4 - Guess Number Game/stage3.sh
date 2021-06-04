ac65a4696ec3430bfa846409d914b5e59a2af540
Nazza01 <nate_thomas_95@hotmail.com>
2021-06-04 18:35:29 +0930

    Working with figlet now!

--- Challenge 4 - Guess Number Game/stage3.sh
+++ Challenge 4 - Guess Number Game/stage3.sh
@@ -2,12 +2,12 @@
 
 # Run stage1.sh - the bash script
 ./stage1.sh
-
+figlet "You Win!!"
 
 # Check if the C program will compile and throw an error if it doensnt run
 if gcc stage2.c -o stage2.out; then
     ./stage2.out
-    echo "You win!! Congratulations :D"
+    figlet "You win!! Congratulations :D"
 else
     echo "Sorry, something went wrong. Please contact me!"
 fi
